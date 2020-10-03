6537c2379237939c3954a41525e33b15f5f30207
sheexiong <chen97xiong@yahoo.com>
2020-10-03 19:11:20 +0800

    Fix error in db config file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    local: {
-        HOST: process.env.POSTGRES_HOST || 'localhost',
-        USER: process.env.POSTGRES_USER || 'postgres',
-        PASSWORD: process.env.POSTGRES_PASS || '1234',
-        DB: process.env.POSTGRES_DB || 'taskb',
-        DIALECT: process.env.POSTGRES_DIALECT || 'postgres',
-        POOL: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
+    HOST: process.env.POSTGRES_HOST || 'localhost',
+    USER: process.env.POSTGRES_USER || 'postgres',
+    PASSWORD: process.env.POSTGRES_PASS || '1234',
+    DB: process.env.POSTGRES_DB || 'taskb',
+    DIALECT: process.env.POSTGRES_DIALECT || 'postgres',
+    POOL: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
     },
 };
